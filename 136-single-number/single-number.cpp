e9d54dacf7e33e8efcb152f8e1af8b6aa3892692
Ankit Gupta <103406821+guptaankitnexis@users.noreply.github.com>
2024-05-10 00:03:48 +0530

    Time: 15 ms (53.73%) | Memory: 19 MB (98.69%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mpp; // Create a map to store the count of each number
-        for(int i = 0 ;i<nums.size();i++){
-            mpp[nums[i]]++; // Increment count for each number
+        int result = 0; // Initialize result to store XOR
+        for(int i = 0; i < nums.size(); i++) {
+            result ^= nums[i]; // Perform XOR operation
         }
-        for (auto it : mpp) { // Iterate through the map
-            if(it.second == 1 ){ // If count is 1, return the number
-                return it.first;
-            }
-        }
-        return -1; // Return -1 if no unique element found
+        return result; // Return the result
     }
 };
