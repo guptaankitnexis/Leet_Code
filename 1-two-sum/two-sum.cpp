e84648c0db86a20ec68ba31a28d59d24af7118c9
Ankit Gupta <103406821+guptaankitnexis@users.noreply.github.com>
2024-05-13 01:03:23 +0530

    Time: 13 ms (55.33%) | Memory: 13.6 MB (44.30%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+        // Map to store the number needed to reach the target and its index
+        map<int, int> mpp;
+        int num;
+        int moreNeeded;
+
+        // Iterate through the input vector
+        for(int i = 0; i < nums.size(); i++) {
+            // Current number from the input vector
+            num = nums[i];
+            // Calculate the number needed to reach the target
+            moreNeeded = target - num;
+
+            // Check if the number needed exists in the map
+            if(mpp.find(moreNeeded) != mpp.end()) {
+                // If found, return the indices of the two numbers
+                // that add up to the target
+                return {mpp[moreNeeded], i};
             }
+            // Otherwise, store the current number and its index in the map
+            mpp[num] = i;
         }
-        return { -1, -1};
+        // If no solution is found, return {-1, -1}
+        return {-1, -1};
     }
 };
