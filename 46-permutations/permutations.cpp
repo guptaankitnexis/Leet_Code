71201b92260f8b758bfb601e6c5fafd0829fe7a5
Ankit Gupta <103406821+guptaankitnexis@users.noreply.github.com>
2024-06-21 14:45:52 +0530

    Time: 2 ms (62.25%) | Memory: 8.7 MB (97.89%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,3 +1,29 @@
+class Solution {
+public:
+    // Function to generate all permutations of the given array
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;      // To store all the permutations
+        int n = nums.size();          // Size of the input array
+        permutation(nums, 0, ans, n); // Start the permutation generation
+        return ans;                   // Return all generated permutations
+    }
+
+    // Recursive function to generate permutations
+    void permutation(vector<int>& nums, int ind, vector<vector<int>>& ans, int n) {
+        if (ind == n) {               // If the current permutation is complete
+            ans.push_back(nums);      // Add it to the answer list
+            return;
+        }
+        for (int i = ind; i < n; i++) { // Iterate over each element starting from `ind`
+            swap(nums[ind], nums[i]);   // Swap the current element with the element at index `ind`
+            permutation(nums, ind + 1, ans, n); // Recurse to continue building the permutation with the next index
+            swap(nums[ind], nums[i]);   // Backtrack: undo the swap to restore the original order
+        }
+    }
+};
+
+/*
+Another approach
 
 class Solution {
 public:
@@ -28,3 +54,5 @@ public:
         }
     }
 };
+
+*/
