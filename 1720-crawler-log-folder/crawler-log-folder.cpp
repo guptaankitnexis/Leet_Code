50a80e3ad619be45b1c1919f8fd582054f27fe00
Ankit Gupta <103406821+guptaankitnexis@users.noreply.github.com>
2024-07-10 14:28:05 +0530

    Time: 4 ms (67.13%) | Memory: 14 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -4,11 +4,9 @@ public:
         int count = 0;  // Initialize a counter to keep track of the current directory level
         
         for(int i = 0; i < logs.size(); i++) {  // Iterate through each log entry
-            if(logs[i] == "../") {  // If the log is "../"
-                if(count > 0) {  // and we're not at the root, go up one level
-                    count--;
-                }
-            } else if(logs[i] != "./") {  // If the log is not "./" (i.e., it is a valid directory change)
+            if(logs[i] == "../" && count > 0) {  // If the log is "../" and we're not at the root, go up one level
+                count--;
+            } else if(logs[i] != "./" && logs[i] != "../") {  // If the log is not "./" and not "../" (i.e., it is a valid directory change)
                 count++;
             }
         }
