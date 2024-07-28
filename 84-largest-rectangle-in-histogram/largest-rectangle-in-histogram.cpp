ed91842ade0d71bcaa9facd8f2ecea97c999e3cd
Ankit Gupta <103406821+guptaankitnexis@users.noreply.github.com>
2024-07-28 20:30:59 +0530

    Time: 98 ms (89.85%) | Memory: 80 MB (68.83%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
-    vector<int> NSE(vector<int> arr, int n) {
-        vector<int> ans(n);
-        stack<int> st;
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-            ans[i] = st.empty() ? n : st.top();
-            st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> PSE(vector<int> arr, int n) {
-        vector<int> ans(n);
-        stack<int> st;
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-            ans[i] = st.empty() ? -1 : st.top();
-            st.push(i);
-        }
-        return ans;
-    }
     int largestRectangleArea(vector<int>& heights) {
-        int area = 0;
         int n = heights.size();
-        vector<int> nse = NSE(heights, n);
-        vector<int> pse = PSE(heights, n);
-        for (int i = 0; i < heights.size(); i++) {
-            area = max(area, heights[i] * (nse[i] - pse[i] - 1));
+        stack <int> st;
+        int maxArea=0;
+        for(int i =0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int element = st.top();
+                st.pop();
+                int nse=i;
+                int pse = st.empty()?-1:st.top();
+                maxArea=max(maxArea,heights[element]*(nse-pse-1));
+            }
+            st.push(i);
         }
-        return area;
+        while(!st.empty()){
+            int nse = n;
+            int element = st.top();
+            st.pop();
+            int pse = st.empty()?-1:st.top();
+            maxArea=max(maxArea,heights[element]*(nse-pse-1));
+        }
+        return maxArea;
     }
 };
